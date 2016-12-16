e8d46d0d735f26fb4fab04695b12b2174c0c31fa
kusnandartoni <kusnandartoni@gmail.com>
2016-12-16 13:29:37 +0700

    ubah ke protected

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 |
 */
 
-require __DIR__.'/private/bootstrap/autoload.php';
+require __DIR__.'/protected/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ require __DIR__.'/private/bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/private/bootstrap/app.php';
+$app = require_once __DIR__.'/protected/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
